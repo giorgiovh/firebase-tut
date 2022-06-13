c5a812cb65f8234ccbc373dcb8dd6ee696e77c6d
giorgiovh <jorgevonhoroch@gmail.com>
2022-06-12 20:15:32 -0400

    first init

---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+// require('dotenv').config();
+import 'dotenv/config.js';
+
+console.log(process.env);
+
+const firebaseConfig = {
+    // apiKey: "AIzaSyAtQTGo1D8uwX0Y5ZyF_wUCGwNHS9AiZ98",
+    apiKey: process.env.API_KEY,
+    authDomain: "fir-tutorial-9a326.firebaseapp.com",
+    projectId: "fir-tutorial-9a326",
+    storageBucket: "fir-tutorial-9a326.appspot.com",
+    messagingSenderId: "389177619879",
+    appId: "1:389177619879:web:44e3220dd47b0f9895f282",
+    measurementId: "G-REVEK8QTT3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
